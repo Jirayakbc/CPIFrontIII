cc457fc0315d01ac31d8c36ef90b2e767585bea2
Jirayakbc <feliperobertorocha@gmail.com>
2023-03-14 23:17:33 -0300

    ajuste crud e css

--- CheckpointI Frontend3/src/App.jsx
+++ CheckpointI Frontend3/src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 //import {format} from 'date-fns'
 import "./styles.css";
 
+//import iconelixeira from "./assets/iconelixeira";
+
 function App(){
 
-  const [count, setCount] = useState(0);
-  
-  const [tituloInput, setTituloInput] = useState("");
+  const [count, setCount] = useState(0); //contador de tarefas
+  const [id, setId] = useState("");
+  const [tituloInput, setTituloInput] = useState(""); //manipular itens formulario
   const [categoria, setCategoria] = useState("");
   const [date, setDate] = useState("");
   const [descricao, setDescricao] = useState("");
 
-  const [listaTarefas, setListaTarefas] = useState([]);
-
+  const [listaTarefas, setListaTarefas] = useState([]);//manipular post formulario
+  //testes de console log
   //console.log({descricao})
   //console.log({date});
   //console.log({tituloInput})
@@ -25,6 +27,9 @@ function App(){
      //   <p>informe os campos</p>
      // }
      alert("Preencha todos os campos!!");
+     function informa(){
+      <>Preencher o titulo</>
+     }
      return;
     }
     
@@ -45,63 +50,102 @@ function App(){
     setCount(count+1);
   }
 
+  function editaTarefa(event){
+    event.preventDefault();
+
+    const copiaListaTarefas = [...listaTarefas];
+
+    const index = copiaListaTarefas.findIndex(
+      (tarefa)=>tarefa.id === id
+    );
+    
+    copiaListaTarefas[index].tituloInput = tituloInput;
+    copiaListaTarefas[index].categoria = categoria;
+    copiaListaTarefas[index].date = date;
+    copiaListaTarefas[index].descricao = descricao;
+
+    setListaTarefas(copiaListaTarefas);
+  }
+
+  function excluirTarefa(id){
+    if(confirm("Excluir tarefa?")){
+      const resposta = listaTarefas.filter((item) => item.id !== id);
+      setListaTarefas(resposta);
+      setCount(count-1);
+    }
+  }
+  function mudaCard(item){
+    setTituloInput(item.tituloInput);
+    setCategoria(item.categoria);
+    setDate(item.date);
+    setDescricao(item.descricao);
+  }
+
   return(
     <div id='main'>
-      <div id="formulario">
+ 
         <div id="formulario-dentro">
-        <h1>Cadastrar Tarefa</h1>
-      <form>
-        <input type="text" 
-        value={tituloInput}
-        onChange={(event) => setTituloInput(event.target.value)}
-        />
+          <div id="formulario-interno">
+           <h1>Cadastrar Tarefa</h1>
+            <form onSubmit={id ? editaTarefa : adicionaTarefas}>
+              <input type="text" 
+              value={tituloInput}
+              onChange={(event) => setTituloInput(event.target.value)}
+              />
 
-        <select
-          value={categoria}
-          onChange={(event) => setCategoria(event.target.value)}
-        >
-          <option value="" >Categoria</option>
-          <option value="Trabalho">Trabalho</option>
-          <option value="Lazer">Lazer</option>
-          <option value="Prioridade">Prioridade</option>
-          <option value="Outros">Outros</option>
-        </select>
-        <br />
-        <input type="date"
-          value={date}
-          onChange={(event) => setDate(event.target.value)}
-          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-        />
-        <br />
-        <textarea name="Descricao" cols="30" rows="10" 
-        value={descricao}
-        onChange={(event) => setDescricao(event.target.value)}
-        ></textarea>
-        <br />
-        <input type="submit" onClick={adicionaTarefas} value="Salvar" />
-      </form>
-        </div>      
+              <select
+                value={categoria}
+                onChange={(event) => setCategoria(event.target.value)}
+              >
+                <option value="" disabled >Categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Lazer">Lazer</option>
+                <option value="Prioridade">Prioridade</option>
+                <option value="Outros">Outros</option>
+              </select>
+              <br />
+              <input type="date"
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
+                pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+              />
+              <br />
+              <textarea name="Descricao"
+                style={{resize: "none"}}
+                id="form-descricao"
+                value={descricao}
+                onChange={(event) => setDescricao(event.target.value)}
+                ></textarea>
+              <br />
+              <input className="save" type="submit"  value={id ? "Salvar" : "Cadastrar"}/>
+          </form>
+        </div> 
       </div>
+
       <div id="lista">
-      <h1 className="tarefas">Minhas tarefas</h1>
-      <h3 id="contador-tarefas">Total: {count} tarefas</h3>
-      {listaTarefas.length > 0 ? (
-        <ul>
-        {listaTarefas.map((item)=>(
-          <>
-            <li key={item.id}>
-            </li><p>{item.tituloInput}</p>
-            <p>{item.categoria}</p>
-            <p>{item.date}</p>
-            <p>{item.descricao}</p>
-            <img src="https://img.icons8.com/external-becris-lineal-becris/256/external-edit-mintab-for-ios-becris-lineal-becris.png" alt="editar" width='20px' />
-            <img src="https://img.icons8.com/ios/256/delete.png" alt="apagar" width='20px'/>
-          </>
-        ))}
-        </ul>
-      ):(
-        <p>Parabéns! Concluiu todas suas tarefas!</p>
-      )}
+        <h1 className="tarefas">Minhas tarefas</h1>
+        <h3 id="contador-tarefas">Total: {count} tarefas</h3>
+        <div id="container-tarefas">
+        {listaTarefas.length > 0 ? (
+          <ul>
+          {listaTarefas.map((item)=>(
+            <div id="select-categorias">
+              <li key={item.id} style={{ listStyleType: "none" }}>
+              </li>
+              <p id="titulo-card">{item.tituloInput}</p>
+              <p id="categoria-card">{item.categoria}</p>
+              <p id="data-card">{item.date}</p>
+              <p id="descricao-card">{item.descricao}</p>
+              <img src="https://img.icons8.com/external-becris-lineal-becris/256/external-edit-mintab-for-ios-becris-lineal-becris.png" alt="editar" width='20px'   onClick={()=>mudaCard(item)}/>
+              <img src="https://img.icons8.com/ios/256/delete.png" alt="apagar" width='20px' onClick={()=>{excluirTarefa(item.id)}}/>
+            </div>
+          ))}
+          </ul>
+        ):(
+          <p>Parabéns! Concluiu todas suas tarefas!</p>
+        )}
+        </div>       
+        
       </div>
     </div>
   )
---
+++ CheckpointI Frontend3/src/assets/iconelixeira.jsx
@@ -0,0 +1,7 @@
+import { Fitrash2 } from 'react-icons/fa';
+
+function iconelixeira(){
+    <Fitrash2 />
+}
+
+export default iconelixeira;
